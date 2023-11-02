a06db2601f6683131a988f09e9b1adf292f2eddd
spookyguyx <v.p204@mail.ru>
2023-11-02 21:23:39 +0300

    slug in table server

--- servers/admin.py
+++ servers/admin.py
@@ -3,5 +3,9 @@ from django.contrib import admin
 from .models import Server
 
 
+class ServerAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"server_slug": ["server_name"]}
+
+
 # Register your models here.
-admin.site.register(Server)
+admin.site.register(Server, ServerAdmin)
