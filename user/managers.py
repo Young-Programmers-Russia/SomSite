322e71b13dfcfe37e50c6860022efb82fb434858
spookyguyx <v.p204@mail.ru>
2023-11-04 01:08:18 +0300

    made user model, made manager.py file, remade tables that used pk users

---
+++ user/managers.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    """
+    Custom user model manager where email is the unique identifiers
+    for authentication instead of usernames.
+    """
+    def create_user(self, email, username, password=None):
+        """
+        Create and save a User with the given email and password.
+        """
+        if not email:
+            raise ValueError(_('The Email must be set'))
+        if not username:
+            raise ValueError(_("User must have an username"))
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password):
+        """
+        Create and save a SuperUser with the given email and password.
+        """
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password,
+            username=username,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
