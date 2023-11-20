77c2633548ac4407d230f53ae9d0e7dd90563c51
Trmtrg <Vitalik.petrackov@yandex.ru>
2023-11-21 01:08:46 +0300

    Partially made multiple mod files upload

--- apps/mods/views.py
+++ apps/mods/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
+from .forms import ModsFileFieldForm
 
 
 class ModUploadPage(TemplateView):
     template_name = "mods/mod_upload.html"
+
+
+class ModsUploadFormView(FormView):
+    form_class = ModsFileFieldForm
+    template_name = 'mods/mods_upload.html'
+    success_url = "..."
+
+    def post(self, request, *args, **kwargs):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        files = form.cleaned_data['file_field']
+        for f in files:
+            ... #Сделать так чтобы файлы создавали свои модели
+        return super().form_valid()
 
 
 class ModsPage(TemplateView):
