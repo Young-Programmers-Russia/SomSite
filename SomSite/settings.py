216a717b5b3be86942813e7b6a17f30a0846f8a2
Trmtrg <Vitalik.petrackov@yandex.ru>
2023-11-24 08:14:01 +0300

    Some stuff

--- SomSite/settings.py
+++ SomSite/settings.py
@@ -132,14 +132,17 @@ STATICFILES_DIRS = [
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'ragexzona@gmail.com'
-EMAIL_HOST_PASSWORD = 'ptks xghi ikbj orkm'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
+# # Email reports
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'ragexzona@gmail.com'
+# EMAIL_HOST_PASSWORD = 'ptks xghi ikbj orkm'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_USE_SSL = False
 # Default primary key field type
+
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
