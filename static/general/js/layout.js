bc4b983460341cb0c3aec06a18bd8a44c95231f3
Danil <kopkov.d@gmail.com>
2023-12-10 21:11:59 +0300

    подправил главную страницу

--- static/general/js/layout.js
+++ static/general/js/layout.js
@@ -1,14 +1,26 @@
+//для окна элементов
 const openBurger = document.getElementById('nav_menu');
-const body = document.getElementById('body');
 const menu = document.getElementById('nav_items');
+//для окна регистрации
+const openLogin = document.getElementById("icon_of_login");
+const Login =document.getElementById('nav_login');
 
-openBurger.addEventListener('click', function(e){
-    e.preventDefault();
-    menu.classList.toggle('active');
-    body.classList.toggle('lock');
-})
+const body = document.getElementById('body');
 
 openBurger.addEventListener('click', function(e){
     e.preventDefault();
     openBurger.classList.toggle('active');
+    menu.classList.toggle('active');
+    body.classList.toggle('lock');
+    openBurger.close('')
+})
+
+
+
+openLogin.addEventListener('click', function(e){
+    e.preventDefault();
+    Login.classList.toggle('active');
+    body.classList.toggle('lock');
+    openLogin.classList.toggle('active');
+
 })
