007d740ce4123ff64719e5d1fb2133988e040290
Данил <kopkov.d@gmail.com>
2024-01-03 02:02:14 +0300

    доделал адаптивность на всех страницах, сделал кнопку "наверх страницы"

--- static/general/js/layout.js
+++ static/general/js/layout.js
@@ -24,3 +24,24 @@ openLogin.addEventListener('click', function(e){
     openLogin.classList.toggle('active');
 
 })
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+  const backToTop = document.getElementById("up_image");
+
+  // Показать/скрыть кнопку при прокрутке страницы
+  window.addEventListener("scroll", function () {
+    if (window.pageYOffset > 300) {
+      backToTop.style.display = "block";
+    } else {
+      backToTop.style.display = "none";
+    }
+  });
+
+  // Плавная прокрутка при клике на кнопку
+  backToTop.addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+});
