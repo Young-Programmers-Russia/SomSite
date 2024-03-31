3a8fe7b63a70106faec2e6f8fe3260f482502556
trmtrg <vitalik.petrackov@yandex.ru>
2024-04-01 01:59:22 +0300

    no to launchers

--- SomSite/apps/launchers/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Launcher
-
-
-@admin.register(Launcher)
-class LauncherAdmin(admin.ModelAdmin):
-    list_display = ['__str__', 'version', 'os']
-    ordering = ['os', '-version']
